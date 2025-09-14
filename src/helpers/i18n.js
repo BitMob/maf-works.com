6a1f12332c745900c34050820dc6887478ebc5d5
maf <13570154+mafmadmaf@users.noreply.github.com>
2025-09-14 21:25:00 +0800

    Update i18n.js

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -9,8 +9,8 @@ export default {
     //en: "gallery of maf's design practice · 2011 - 2020",
   },
   more: {
-    en: "more",
-    zh: "更多",
+    en: "menu",
+    zh: "menu",
   },
   fold: {
     en: "fold",
